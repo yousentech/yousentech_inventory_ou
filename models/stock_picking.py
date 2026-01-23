26af9eb8137eb91d4d0e1a9eca487330f83baa65
yousentech <83219678+yousentech@users.noreply.github.com>
2026-01-23 22:31:03 +0300

    update

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -22,78 +22,78 @@ class StockPicking(models.Model):
         )
 
 
-    from_other_order = fields.Boolean(
-        string='From Other Document',
-        compute='_compute_from_other_document',
+    # from_other_order = fields.Boolean(
+    #     string='From Other Document',
+    #     compute='_compute_from_other_document',
      
-    )
+    # )
 
-    @api.depends('sale_id', 'purchase_id', 'move_ids_without_package')
-    def _compute_from_other_document(self):
-        for picking in self:
-            picking.from_other_order = bool(
-                picking.sale_id
-                or picking.purchase_id
-                or  picking._is_return_picking()
+    # @api.depends('sale_id', 'purchase_id', 'move_ids_without_package')
+    # def _compute_from_other_document(self):
+    #     for picking in self:
+    #         picking.from_other_order = bool(
+    #             picking.sale_id
+    #             or picking.purchase_id
+    #             or  picking._is_return_picking()
 
-            )
+    #         )
  
-    allowed_ou_domain = fields.Char(compute="get_allowed_ou_domain")
+    # allowed_ou_domain = fields.Char(compute="get_allowed_ou_domain")
 
-    @api.depends('company_id','invoice_user_id')
-    def get_allowed_ou_domain(self):
-        for rec in self:
-            rec.allowed_ou_domain = [('id','in',self.env.user.ou_config_ids.filtered(lambda x: x.company_id.id == rec.company_id.id).allowed_ou_ids.ids)]
+    # @api.depends('company_id','invoice_user_id')
+    # def get_allowed_ou_domain(self):
+    #     for rec in self:
+    #         rec.allowed_ou_domain = [('id','in',self.env.user.ou_config_ids.filtered(lambda x: x.company_id.id == rec.company_id.id).allowed_ou_ids.ids)]
 
-    @api.model
-    def default_get(self, fields_list):
-        res = super().default_get(fields_list)
+    # @api.model
+    # def default_get(self, fields_list):
+    #     res = super().default_get(fields_list)
 
-        company_id = res.get('company_id', self.env.company.id)
+    #     company_id = res.get('company_id', self.env.company.id)
 
-        if not res.get('operation_unit_id'):
-            ou = self.env.user.ou_config_ids.filtered(
-                lambda x: x.company_id.id == company_id
-            ).default_ou_id
+    #     if not res.get('operation_unit_id'):
+    #         ou = self.env.user.ou_config_ids.filtered(
+    #             lambda x: x.company_id.id == company_id
+    #         ).default_ou_id
 
-            if ou:
-                res['operation_unit_id'] = ou.id
+    #         if ou:
+    #             res['operation_unit_id'] = ou.id
 
-        return res
+    #     return res
     
-    @api.onchange('company_id')
-    def _onchange_company_id_set_ou(self):
-        for rec in self:
-            if not rec.company_id:
-                rec.operation_unit_id = False
-                return
+    # @api.onchange('company_id')
+    # def _onchange_company_id_set_ou(self):
+    #     for rec in self:
+    #         if not rec.company_id:
+    #             rec.operation_unit_id = False
+    #             return
 
-            # OU الحالي غير تابع للشركة
-            if rec.operation_unit_id and rec.operation_unit_id.company_id != rec.company_id:
-                rec.operation_unit_id = False
+    #         # OU الحالي غير تابع للشركة
+    #         if rec.operation_unit_id and rec.operation_unit_id.company_id != rec.company_id:
+    #             rec.operation_unit_id = False
 
-            # تعيين OU افتراضي
-            if not rec.operation_unit_id:
-                ou = self.env.user.ou_config_ids.filtered(
-                    lambda x: x.company_id == rec.company_id
-                ).default_ou_id
+    #         # تعيين OU افتراضي
+    #         if not rec.operation_unit_id:
+    #             ou = self.env.user.ou_config_ids.filtered(
+    #                 lambda x: x.company_id == rec.company_id
+    #             ).default_ou_id
 
-                if ou:
-                    rec.operation_unit_id = ou
+    #             if ou:
+    #                 rec.operation_unit_id = ou
 
-    allow_modify_ou_flag = fields.Boolean(
-        default=lambda self: self._default_allow_modify_ou_flag(),
-        compute="_check_allow_modify_ou_flag",
-    )
+    # allow_modify_ou_flag = fields.Boolean(
+    #     default=lambda self: self._default_allow_modify_ou_flag(),
+    #     compute="_check_allow_modify_ou_flag",
+    # )
 
-    def _default_allow_modify_ou_flag(self):
+    # def _default_allow_modify_ou_flag(self):
         
-        return self.user_has_groups('yousentech_invoicing_ou.group_allow_modify_ou')
+    #     return self.user_has_groups('yousentech_invoicing_ou.group_allow_modify_ou')
 
-    def _check_allow_modify_ou_flag(self):
+    # def _check_allow_modify_ou_flag(self):
         
-        for rec in self:
-            rec.allow_modify_ou_flag = self.user_has_groups('yousentech_invoicing_ou.group_allow_modify_ou')
+    #     for rec in self:
+    #         rec.allow_modify_ou_flag = self.user_has_groups('yousentech_invoicing_ou.group_allow_modify_ou')
 
     @api.model
     def create(self, vals):
@@ -148,25 +148,25 @@ class StockPicking(models.Model):
                             'operation_unit_id': picking.operation_unit_id.id
                         })
 
-    @api.onchange('operation_unit_id')
-    def _onchange_operation_unit_id_sync_returns(self):
-        for picking in self:
-            if not picking.operation_unit_id:
-                continue
+    # @api.onchange('operation_unit_id')
+    # def _onchange_operation_unit_id_sync_returns(self):
+    #     for picking in self:
+    #         if not picking.operation_unit_id:
+    #             continue
 
-            # لا تطبق على المرتجع نفسه
-            if picking._is_return_picking():
-                continue
+    #         # لا تطبق على المرتجع نفسه
+    #         if picking._is_return_picking():
+    #             continue
 
-            # المرتجعات التابعة
-            return_pickings = self.env['stock.picking'].search([
-                ('move_ids_without_package.origin_returned_move_id.picking_id', '=', picking.id),
-            ])
+    #         # المرتجعات التابعة
+    #         return_pickings = self.env['stock.picking'].search([
+    #             ('move_ids_without_package.origin_returned_move_id.picking_id', '=', picking.id),
+    #         ])
 
-            for return_picking in return_pickings:
-                return_picking.operation_unit_id = picking.operation_unit_id
-                for layer in  return_picking.move_ids_without_package.stock_valuation_layer_ids:
-                    layer.account_move_id.write({'operation_unit_id': picking.operation_unit_id.id})
+    #         for return_picking in return_pickings:
+    #             return_picking.operation_unit_id = picking.operation_unit_id
+    #             for layer in  return_picking.move_ids_without_package.stock_valuation_layer_ids:
+    #                 layer.account_move_id.write({'operation_unit_id': picking.operation_unit_id.id})
 
 class StockMove(models.Model):
     _inherit = 'stock.move' 
